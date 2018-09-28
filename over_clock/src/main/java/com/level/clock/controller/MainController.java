ce7c09c67f44b7ea8d7e94420817bbb5ba07dd19
jun <gkdlwkqk@gmail.com>
2018-09-29 00:30:12 +0900

    go

--- over_clock/src/main/java/com/level/clock/controller/MainController.java
+++ over_clock/src/main/java/com/level/clock/controller/MainController.java
@@ -1,14 +1,15 @@
 package com.level.clock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping(value="/main")
-	public String main() {
-		System.out.println("!!!");
+	public String main(Model m) {
+		m.addAttribute("center", "center.jsp");
 		return "main";
 	}
 }
